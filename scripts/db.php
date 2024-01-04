c57e741847ff91d7c5bc3ad70487bc57cb98b0a6
lolnuggets <46546150+lolnuggets@users.noreply.github.com>
2024-01-03 21:00:35 -0800

    Update db.php
    removed the host ip & password 4 security reasons

--- scripts/db.php
+++ scripts/db.php
@@ -1,10 +1,10 @@
 <?php
 		
-$host="db-dnd.cd6nxpmmcv4n.us-east-2.rds.amazonaws.com";
+$host="localhost";
 $port=3306;
 $socket="";
 $user="admin";
-$password="cNdsxmcWwyxfK#!V4F4EyT7q&L";
+$password="";
 $dbname="dnddb";
 
 $con = new mysqli($host, $user, $password, $dbname, $port, $socket);
