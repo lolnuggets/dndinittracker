4acaf44b9d37e14f328c3d10f3caa3e67e3c138d
lolnuggets <46546150+lolnuggets@users.noreply.github.com>
2024-01-03 21:01:16 -0800

    Update db.php
    removed securtity password for databae

--- scripts/db.php
+++ scripts/db.php
@@ -32,15 +32,7 @@ if ($con->connect_error) {
 
 PUBLIC
 <?php
-		
-$host="db-dnd.cd6nxpmmcv4n.us-east-2.rds.amazonaws.com";
-$port=3306;
-$socket="";
-$user="admin";
-$password="cNdsxmcWwyxfK#!V4F4EyT7q&L";
-$dbname="dnddb";
-
-$con = new mysqli($host, $user, $password, $dbname, $port, $socket);
+		 $dbname, $port, $socket);
 if ($con->connect_error) {
   die("Connection failed: " . $con->connect_error);
 }
